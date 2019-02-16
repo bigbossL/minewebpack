07fc2164925bf34f4a3ecd27ecc8c8be5ecf884a
liujiarong <451458495@qq.com>
2019-02-16 16:28:14 +0800

    脚手架首次提交

---
+++ webpack/webpack.build.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin"); //css分离工具
+const ProgressBarPlugin = require("progress-bar-webpack-plugin");//打包显示进度条
+const HtmlWebpackPlugin = require('html-webpack-plugin');//动态生成html
+const CleanWebpackPlugin =require('clean-webpack-plugin');//每次打包删除之前的文件
+// const WebpackDevServer=require('webpack-dev-server');//这个是开发用的服务器
+module.exports = {
+    entry: path.resolve(__dirname , '../src/index.tsx'),
+    output: {
+        path:path.resolve(__dirname ,'../dist'),
+        filename: 'js/[name]_[hash].js'
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    // 注意 1
+                    fallback: {
+                        loader: "style-loader"
+                    },
+                    use: [{
+                            loader: "css-loader",
+                        },
+                        {
+                            loader: "sass-loader"
+                        }
+                    ]
+                })
+            }
+
+        ]
+    },
+//     //配置开发服务器
+//     devServer: {
+//     // contentBase: [path.join(__dirname, 'public'), path.join(__dirname, 'assets')],
+//     compress:true,
+//     port: 8080,
+//     hot:true,
+//     inline:true,
+    
+//   },
+    plugins: [
+        new ExtractTextPlugin("css/[name]_[hash].css"),
+        new ProgressBarPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname ,'../public/index.html'),
+            inject:'body',
+        }),
+        new CleanWebpackPlugin(['../dist']),
+        // new WebpackDevServer()
+    ]
+
+
+}
